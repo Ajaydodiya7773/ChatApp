011ec9a2e4abb9d66e361a067f278b4df2b2920f
Ajaydodiya <ajaysinghdodiya29@gmail.com>
2024-09-02 15:54:55 +0530

    DataBase conncetion or backend connected

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+
+const app = express()
+dotenv.config();
+
+const PORT =process.env.PORT || 5000;
+const URI = process.env.MOGODB_URI;
+try {
+    mongoose.connect(URI);
+    console.log("Connected to MongoDB");
+} catch (error) {
+    
+}
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
