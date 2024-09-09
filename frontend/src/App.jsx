f933577bbb852acf3a225df0f18953bed80c00a0
Ajaydodiya <ajaysinghdodiya29@gmail.com>
2024-09-09 23:26:15 +0530

    Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
 
-import SideBar from "./Home/Left/SideBar";
-import Chats from "./Home/Right/Chats";
-import Logout from "./Home/Left1/Logout";
+// import SideBar from "./Home/Left/SideBar";
+// import Chats from "./Home/Right/Chats";
+// import Logout from "./Home/Left1/Logout";
+
+import Signup from "./componant/signup";
+
+
 
 function App() {
   return (
     <>
-      <div className=" flex h-screen">
+      {/* <div className=" flex h-screen">
         <Logout />
         <SideBar />
         <Chats />
-      </div>
+      </div> */}
+    <Signup/>
     </>
+
   );
 }
 
